4c9f46bad633f0b4571e9f51875d99d6b2e77309
Julius Caamic <julius@greenbrainer.com>
2016-02-04 10:45:39 +0800

    add new controller for download ebook

---
+++ mysite/code/controllers/DownloadController.php
@@ -0,0 +1,21 @@
+<?php
+class DownloadController extends Controller {
+
+	private static $allowed_actions = array('file');
+
+	public function init() {
+		parent::init();
+	}
+
+	public function file() {
+		$filename = Convert::raw2sql($this->request->param('ID'));
+		if ($filename) {
+			$file = File::get()->filter('Name', $filename . '.pdf');
+			if ($file->exists()) {
+				return SS_HTTPRequest::send_file(file_get_contents(Director::absoluteBaseURL() . $file->Filename), $file->Name);
+			}
+		}
+
+		return false;
+	}
+}
